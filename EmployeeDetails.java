e1e186aae4d502a89efeda3908de1bbd589e4afa
suchith001 <suchithpkd@gmail.com>
2026-01-31 22:11:59 +0530

    Add files via upload
    Day1

---
+++ EmployeeDetails.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String name = "John Doe";
+		 System.out.println("Employee name is:"+ name);
+		 int age = 30;
+		 System.out.println("Employee age is:"+ age);
+		 double salary = 55000.75;
+		 System.out.println("Employee salary is:"+ salary);
+		 char gender = 'M';
+		 System.out.println("Employee gender is:"+ gender);
+		 boolean acceptTerms = true;
+		 System.out.println("Employee acceptTerms:"+ acceptTerms);
+		 float rating = 4.5f;
+		 System.out.println("Employee rating is:"+ rating);
+		 long mobileNumber = 9876543210L;
+		 System.out.println("Employee mobilenumber is:"+ mobileNumber);
+		 long pinCode = 56001;
+		 System.out.println("Employee Pincode is:"+ pinCode);
+		 int luckyNumber = 7;
+		 System.out.println("Employee luckynumber is:"+ luckyNumber);
+	}
+
+}
